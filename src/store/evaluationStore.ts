bf321897acc33454e91f62a14bdf648607c7d499
Fabian Bösel <fabian.boesel@web.de>
2025-01-19 18:30:59 +0100

    fix frontend type usage

--- src/store/evaluationStore.ts
+++ src/store/evaluationStore.ts
@@ -1,39 +1,39 @@
-import { create } from 'zustand';
-import type { EvaluationState } from '../types';
+import { create } from "zustand";
+import type { ChatMessage, EvaluationState, LikertScale } from "../types";
 
 interface EvaluationStore extends EvaluationState {
-  setStep: (step: EvaluationState['currentStep']) => void;
-  setPreSurveyResponse: (questionId: string, response: number) => void;
-  setPostSurveyResponse: (questionId: string, response: number) => void;
-  addChatMessage: (message: Omit<ChatMessage, 'id' | 'timestamp'>) => void;
+  setStep: (step: EvaluationState["currentStep"]) => void;
+  setPreSurveyResponse: (questionId: string, response: LikertScale) => void;
+  setPostSurveyResponse: (questionId: string, response: LikertScale) => void;
+  addChatMessage: (message: Omit<ChatMessage, "id" | "timestamp">) => void;
   startEvaluation: () => void;
   endEvaluation: () => void;
 }
 
 export const useEvaluationStore = create<EvaluationStore>((set) => ({
-  currentStep: 'landing',
+  currentStep: "landing",
   preSurveyResponses: {},
   postSurveyResponses: {},
   chatHistory: [],
-  
+
   setStep: (step) => set({ currentStep: step }),
-  
-  setPreSurveyResponse: (questionId, response) =>
+
+  setPreSurveyResponse: (questionId, response: LikertScale) =>
     set((state) => ({
       preSurveyResponses: {
         ...state.preSurveyResponses,
         [questionId]: response,
       },
     })),
-    
-  setPostSurveyResponse: (questionId, response) =>
+
+  setPostSurveyResponse: (questionId, response: LikertScale) =>
     set((state) => ({
       postSurveyResponses: {
         ...state.postSurveyResponses,
         [questionId]: response,
       },
     })),
-    
+
   addChatMessage: (message) =>
     set((state) => ({
       chatHistory: [
@@ -45,7 +45,7 @@ export const useEvaluationStore = create<EvaluationStore>((set) => ({
         },
       ],
     })),
-    
+
   startEvaluation: () => set({ startTime: new Date() }),
   endEvaluation: () => set({ endTime: new Date() }),
 }));
