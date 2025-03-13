0e9a3dd4336faf752ab3d23431d2c3d7a6edb952
Fabian Bösel <fabian.boesel@web.de>
2025-03-13 02:55:16 +0100

    fix api url to prod server

--- src/store/evaluationStore.ts
+++ src/store/evaluationStore.ts
@@ -103,7 +103,7 @@ interface EvaluationStore extends EvaluationState {
   checkNextTopic: () => Promise<void>;
 }
 
-const API_BASE = "http://localhost:5000/api"; //"http://137.250.171.247:5000/api";
+const API_BASE = "http://137.250.171.247:5000/api"; // "http://localhost:5000/api";
 
 export const useEvaluationStore = create<EvaluationStore>((set, get) => ({
   currentStep: "landing",
